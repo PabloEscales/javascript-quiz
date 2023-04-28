09536fdc35978633aed0946192f59795e59c2ddd
Pablo Escales <pabloescales@gmail.com>
2023-04-28 16:14:01 +0200

    link updated

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,7 +1,5 @@
 export const getAllQuestions = async () => {
-  const res = await fetch('http://localhost:5173/data.json')
+  const res = await fetch('http://microsoft-quiz.vercel.app/data.json')
   const json = await res.json()
   return json
 }
-
-
